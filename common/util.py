34748b678e09c237210a204b3980e25ddc58d2bc
youfeng <youfeng@haizhi.com>
2017-10-10 15:16:17 +0800

    修正拦截url

--- common/util.py
+++ common/util.py
@@ -264,6 +264,7 @@ def judge_feature(content):
     feature_list = ['val2.bangruitech.com',
                     'val.bangruitech.com',
                     'valbr.bangruitech.com',
+                    'valyd.bangruitech.com',
                     '请不要使用非法的URL地址访问',
                     '您正在试图非法攻击',
                     '您访问的URL地址不被允许']
